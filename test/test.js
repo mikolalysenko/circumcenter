89f405d8dcd062624a06ee18cd8e0c6603da3728
Mikola Lysenko <mikolalysenko@gmail.com>
2014-06-02 18:20:24 -0500

    tap -> tape

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 var circumcenter = require("../circumcenter.js")
 
-require("tap").test("circumcenter", function(t) {
+require("tape").test("circumcenter", function(t) {
 
   t.equals(circumcenter([[1], [2]]).join(","), [1.5].join(","))
   t.equals(circumcenter([[0,0], [0,1], [1,0]]).join(","), [0.5,0.5].join(","))
